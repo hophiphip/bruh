645ecbf425285909ea5c1e857d9bdccdbd74c424
hophiphip <nighop@yandex.ru>
2022-01-26 00:21:32 +0400

    Docker stuff

---
+++ README.md
@@ -0,0 +1,14 @@
+# Bruh
+
+## Requirements
+- [Docker](https://www.docker.com/)
+
+## Quick start
+Create a config file.
+```shell
+cp .env.example .env
+```
+Start the app
+```shell
+docker-compose up
+```
--- bruh/config/database.php
+++ bruh/config/database.php
@@ -65,12 +65,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('POSTGRES_DB_URI'),
+            'host' => env('POSTGRES_DB_HOST', '127.0.0.1'),
+            'port' => env('POSTGRES_DB_PORT', '5432'),
+            'database' => env('POSTGRES_DB_DATABASE', 'forge'),
+            'username' => env('POSTGRES_DB_USERNAME', 'forge'),
+            'password' => env('POSTGRES_DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
