f827255038e17f4c4b6e519e402911cd18218ba9
hophiphip <nighop@yandex.ru>
2022-01-29 22:39:16 +0400

    Added & configured MongoDB as a dependency

--- bruh/config/app.php
+++ bruh/config/app.php
@@ -175,6 +175,10 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        /*
+         * MongoDB
+         */
+        Jenssegers\Mongodb\MongodbServiceProvider::class,
     ],
 
     /*
