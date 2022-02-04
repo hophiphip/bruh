e87e522393839e3bae0047e54c17cc655e59672a
hophiphip <nighop@yandex.ru>
2022-02-04 23:48:38 +0400

    blade 🗡️

---
+++ bruh/resources/views/offers/index.blade.php
@@ -0,0 +1,11 @@
+@extends('main')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <offers></offers>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ bruh/resources/views/offers/new.blade.php
@@ -0,0 +1,11 @@
+@extends('main')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <offer-form></offer-form>
+            </div>
+        </div>
+    </div>
+@endsection
