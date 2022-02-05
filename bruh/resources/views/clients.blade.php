41b2c4789c9097ba5250381b7272f23403062da0
hophiphip <nighop@yandex.ru>
2022-02-05 23:10:23 +0400

    Client page

---
+++ bruh/resources/views/clients.blade.php
@@ -0,0 +1,9 @@
+@extends('templates.index')
+
+@section('content')
+    <clients-page clients-url="/clients"
+                  insurers-url="/insurers"
+                  sign-in-url="/clients/sign-in"
+                  sign-up-url="/clients/sign-up">
+    </clients-page>
+@endsection
