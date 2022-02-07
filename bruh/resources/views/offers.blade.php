8343e0bb3084165fd95a997e0fa4713ac3d0b012
hophiphip <nighop@yandex.ru>
2022-02-07 11:03:46 +0400

    Added even worse layout

--- bruh/resources/views/offers.blade.php
+++ bruh/resources/views/offers.blade.php
@@ -22,11 +22,15 @@
                     </div>
                 </form>
 
+                <br/>
+
+                <!-- TODO: Need card of whatever/ and mb. do not submit form page each time but do REST request and update via JS in Vue -->
                 @forelse ($offers as $offer)
                     <article class="mb-3">
-                        <h2>{{ $offer->name }}</h2>
-                        <p class="m-0">{{ $offer->company }}</p>
-                        <h3 class="m-0">{{ $offer->description }}</h3>
+                        <h4>Case: {{ $offer->getCaseName() }}</h4>
+                        <p class="m-0">Company: {{ $offer->getCompanyName() }}</p>
+                        <h5 class="m-0">{{ $offer->description }}</h5>
+                        <br/>
                     </article>
                 @empty
                     <p>No offers found</p>
