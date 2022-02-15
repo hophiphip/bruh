bc5115fe46bb7c7ce4c3140f7b4b79d504ffb50a
hophiphip <nighop@yandex.ru>
2022-02-15 23:05:38 +0400

    New header

---
+++ bruh/resources/views/shared/header.blade.php
@@ -0,0 +1,11 @@
+<header class="header">
+    <a class="logo" href="/">
+        <div>bruh!</div>
+    </a>
+
+    <div class="padding"></div>
+
+    <a class="for-insurers" href="/insurer">
+        <button>For insurers</button>
+    </a>
+</header>
