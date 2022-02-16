d97beb255278d63e41f52671e1e5df60c7e172d7
hophiphip <nighop@yandex.ru>
2022-02-16 23:59:10 +0400

    Added main template

--- bruh/resources/views/templates/main.blade.php
+++ bruh/resources/views/templates/main.blade.php
@@ -6,38 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
 
-    <title>Bruh</title>
+    <title>{{ $title }}</title>
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css">
+
+    @yield('styles')
 </head>
 
 <body>
-<div id="app">
-    <nav class="navbar navbar-findcond">
-
-        <div class="container">
-            <div class="navbar-header">
-
-                <!-- TODO: route/mix is BAD in prod -->
-                <!-- For later: https://stackoverflow.com/questions/29912997/laravel-routes-behind-reverse-proxy -->
-
-                <a class="navbar-brand" href="{{ route('index') }}">Home page</a>
-            </div>
-
-            <div class="collapse navbar-collapse" id="navbar">
-                <ul class="nav navbar-nav navbar-right">
-                    <li class="active">
-                        <a href="{{ route('new') }}">Observe offers</a>
-                    </li>
-                </ul>
-            </div>
-
-        </div>
-
-    </nav>
     @yield('content')
-</div>
-<script src="{{ mix('js/app.js') }}"></script>
 </body>
 </html>
