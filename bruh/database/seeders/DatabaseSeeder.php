480f28939425170f7dac6abb4dec060c44a19fbc
hophiphip <nighop@yandex.ru>
2022-02-17 11:44:29 +0400

    Updated seeder for new models structure

--- bruh/database/seeders/DatabaseSeeder.php
+++ bruh/database/seeders/DatabaseSeeder.php
@@ -4,11 +4,19 @@ namespace Database\Seeders;
 
 use App\Models\Insurer;
 use App\Models\Offer;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
+    /**
+     * Seed count.
+     *
+     * @var integer
+     */
+    public const SEED_COUNT = 20;
+
     /**
      * Seed the application's database.
      *
@@ -19,9 +27,13 @@ class DatabaseSeeder extends Seeder
         DB::table(app(Offer::class)->getTable())->truncate();
         DB::table(app(Insurer::class)->getTable())->truncate();
 
-        Insurer::factory()->count(20)->create()->each(function ($insurer) {
-            $offers = Offer::factory()->count(5)->make();
-            $insurer->getOffers()->saveMany($offers);
+        User::factory()->count(self::SEED_COUNT)->create()->each(function ($user) {
+            Insurer::factory()->count(1)->create()->each(function ($insurer) use ($user) {
+                $offers = Offer::factory()->count(5)->make();
+                /* TODO: User::find(1)->insurers() returns nothing ? */
+                $insurer->getOffers()->saveMany($offers);
+                $user->insurer()->save($insurer);
+            });
         });
     }
 }
