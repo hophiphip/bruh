f3d2aa0a3bf7043e6954eed3b30773a73aa37ff0
hophiphip <nighop@yandex.ru>
2022-02-17 11:42:13 +0400

    Added login token for user

---
+++ bruh/app/Models/LoginToken.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class LoginToken extends Model
+{
+    use HasFactory;
+
+    /**
+     * @var string $table login token table name
+     */
+    protected $table = 'login_tokens';
+
+    protected $guarded = [];
+
+    protected $dates = [
+        'expires_at', 'consumed_at',
+    ];
+
+    /**
+     * Get owner of this session token.
+     *
+     * @return BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Test whether token is valid.
+     *
+     * @return bool
+     */
+    public function isValid(): bool
+    {
+        return !$this->isExpired() && !$this->isConsumed();
+    }
+
+    /**
+     * Test whether token is expired.
+     *
+     * @return bool
+     */
+    public function isExpired(): bool
+    {
+        return $this->expires_at->isBefore(now());
+    }
+
+    /**
+     * Test whether token was consumed.
+     *
+     * @return bool
+     */
+    public function isConsumed(): bool
+    {
+        return $this->consumed_at !== null;
+    }
+
+    /**
+     * Consume token.
+     */
+    public function consume()
+    {
+        $this->consumed_at = now();
+        $this->save();
+    }
+}
