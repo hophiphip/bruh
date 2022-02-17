a6e784be7baef8cac056513f2b703e8b01eddd8e
hophiphip <nighop@yandex.ru>
2022-02-17 15:55:50 +0400

    Added seed constants

--- bruh/database/seeders/DatabaseSeeder.php
+++ bruh/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\Insurer;
+use App\Models\LoginToken;
 use App\Models\Offer;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -11,11 +12,18 @@ use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
-     * Seed count.
+     * Seed count for users.
      *
      * @var integer
      */
-    public const SEED_COUNT = 20;
+    public const USER_SEED_COUNT = 20;
+
+    /**
+     * Seed count for offers.
+     *
+     * @var integer
+     */
+    public const OFFER_SEED_COUNT = 5;
 
     /**
      * Seed the application's database.
@@ -26,10 +34,13 @@ class DatabaseSeeder extends Seeder
     {
         DB::table(app(Offer::class)->getTable())->truncate();
         DB::table(app(Insurer::class)->getTable())->truncate();
+        DB::table(app(LoginToken::class)->getTable())->truncate();
+        DB::table(app(User::class)->getTable())->truncate();
 
-        User::factory()->count(self::SEED_COUNT)->create()->each(function ($user) {
+        User::factory()->count(self::USER_SEED_COUNT)->create()->each(function ($user) {
             Insurer::factory()->count(1)->create()->each(function ($insurer) use ($user) {
-                $offers = Offer::factory()->count(5)->make();
+                $offers = Offer::factory()->count(self::OFFER_SEED_COUNT)->make();
+
                 /* TODO: User::find(1)->insurers() returns nothing ? */
                 $insurer->getOffers()->saveMany($offers);
                 $user->insurer()->save($insurer);
