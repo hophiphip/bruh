cd304f199c94268171f0c2d554a39270653ae33c
hophiphip <nighop@yandex.ru>
2022-02-20 14:35:08 +0400

    Added login button

--- bruh/resources/views/shared/header.blade.php
+++ bruh/resources/views/shared/header.blade.php
@@ -5,6 +5,10 @@
 
     <div class="padding"></div>
 
+    <a class="for-log-in" href="{{ \App\Providers\RouteServiceProvider::LOGIN }}">
+        <button>Log In</button>
+    </a>
+
     <a class="for-insurers" href="{{ \App\Providers\RouteServiceProvider::INSURER }}">
         <button>For insurers</button>
     </a>
