4ad26fd583ff31cc9b9ee1d9d7e96fd20209f819
hophiphip <nighop@yandex.ru>
2022-02-27 14:41:34 +0400

    Offer Request instead of Request

---
+++ bruh/database/migrations/2022_02_27_101737_offer_request.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Models\Offer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class OfferRequest extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(app(OfferRequest::class)->getTable(), function (Blueprint $table) {
+           $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('offer_id');
+            $table->foreign('offer_id')->references('id')->on(app(Offer::class)->getTable())->cascadeOnDelete();
+
+            $table->string('email');
+            $table->timestamp('email_verified_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(app(OfferRequest::class)->getTable());
+    }
+}
