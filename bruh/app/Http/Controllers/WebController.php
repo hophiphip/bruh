da242923c3b1ec4d4f01b3aebfb73860297e2d32
hophiphip <nighop@yandex.ru>
2022-02-28 00:22:48 +0400

    offer

--- bruh/app/Http/Controllers/WebController.php
+++ bruh/app/Http/Controllers/WebController.php
@@ -5,10 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Insurer;
 use App\Models\Offer;
 use App\Models\OfferRequest;
+use App\Models\User;
+use App\Providers\RouteServiceProvider;
 use App\Services\Interfaces\SearchServiceInterface;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Contracts\View\View;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Redirector;
 
 
 class WebController extends Controller
@@ -33,6 +38,29 @@ class WebController extends Controller
         return view('offers', [
             'offers' => $offers,
         ]);
+    }
+
+    public function offer(int $id): Factory|View|Application
+    {
+        $offer = Offer::whereId($id)->firstOrFail();
+
+        return view('offer', [
+            'offer' => $offer,
+        ]);
+    }
+
+    public function offerRequestSubmit(Request $request): Redirector|Application|RedirectResponse
+    {
+        $submit = $request->validate([
+            'email' => [ 'required', 'email' ],
+            'captcha' => 'required|captcha',
+        ]);
+
+        /* TODO: Store email-client info in MongoDB for statistics */
+
+        session()->flash('success', true);
+
+        return back();
     }
 
     public function gettingStarted(): Factory|View|Application
